0529302615afdcd9f910a495425bcd47ea86a5b8
Jasmine Lu <xjasminelu@gmail.com>
2026-09-07 17:49:46 -0500

    fleshed out readme and running project 1 info

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # COMP150_F26_Project1
 Template code for COMP150_F26_Project1
+
+## Team
+
+## Files
+`project1.py` - the file you need to edit as a team. You'll need to write code for the `encode()` and `decode()` functions. You are also expected to write helper functions to help you solve ciphered messages. 
+`test.py` - a file that runs some quick tests on your project1.py functions. Might be helpful if you're not sure what is going wrong.
+
+## How to run
+You can run your python file via the command line (on your terminal)
+
+`python3 project1.py encode "TEST MESSAGE"` will run the encode function
+
+`python3 project1.py decode "TEST MESSAGE" "{test_dict: 'value'}"` will run the decode function
--- project1.py
+++ project1.py
@@ -1,6 +1,7 @@
 # Project 1 Template Code
 # Written by Jasmine Lu 2026
 
+import sys
 
 '''
  Takes a message and uses a ‘secret’ cipher to encode the message
@@ -29,6 +30,15 @@ def decode(encoded_msg, cipher):
 # Consider what letters might be the most common in messages and might be easy to identify even when in a secret code
 # Feel free to look up strategies that help with these cipher puzzles
 # Make sure to write a comment explaining how your helper functions work
+# Feel free to create functions other than helper() (like a helper1, helper2, etc. and name them whatever you want)
 
 def helper(encoded_msg):
 	print("Modify this function to actually be helpful in decoding this message: " + encoded_msg)
+
+
+# No need to change these last sections. They are just for easily running your functions via the command line.
+if __name__ == "__main__":
+    if sys.argv[1] == 'encode':
+    	print(encode(sys.argv[2]))
+    elif sys.argv[1] == 'decode':
+    	print(decode(sys.argv[2], eval(sys.argv[3])))
